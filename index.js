945761053ff9dc5239894123862c0e0ee12c4eb1
Mic Black <mic@micslab.com>
2019-08-28 06:40:04 +1000

    Stage 2: UI design and layout with placeholder data

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const vm = new window.Vue({
-	
+
 	el: '#app',
 
 	data() {
-		return {}
+		return {
+			doggos: [],
+			maxDoggos: 0,
+			doggo: {
+				name: 'Test Doggo Name',
+				age: 0,
+				locality: 'Test Locality',
+				gender: 'Female',
+				photo: 'https://placedog.net/1280/720?random'
+			},
+			index: 0
+		}
 	},
 
 	methods: {},
